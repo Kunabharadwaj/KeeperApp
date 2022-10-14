333f02f5813e1713c3ba93e7fa40950602181ac2
Bharadwaj <kunabharadwaj520@gmail.com>
2022-10-14 22:56:30 +0100

    Note component is added

--- keeperapp/src/Components/App.jsx
+++ keeperapp/src/Components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Note from "./Note";
 import "../App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Header />
       <Footer />
+      <Note />
     </div>
   );
 }
